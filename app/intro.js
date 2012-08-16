931c7e9d6e646910c51fe9d8ec2ee129fb8bb952
Mihai Parparita <mihaip@chromium.org>
2012-08-16 14:59:59 -0700

    Use an explicit intent when we want to invoke ourselves.

--- app/intro.js
+++ app/intro.js
@@ -5,8 +5,14 @@ onload = function() {
   document.getElementById('intent-form').onsubmit = function(event) {
     var feedUrl = feedUrlNode.value;
 
-    var intent = new WebKitIntent(
-        'http://webintents.org/view', 'application/atom+xml', feedUrl);
+    var intent = new WebKitIntent({
+      action: 'http://webintents.org/view',
+      type: 'application/atom+xml',
+      data: feedUrl,
+      // Use an explicit intent so that we don't trigger the picker, and instead
+      // we handle it directly.
+      service: location.protocol + '//' + location.hostname + '/main.html'
+    });
 
     window.navigator.webkitStartActivity(intent);
 
